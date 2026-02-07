8c033faa8bad3c45ea81a4871c1d9f4931a82b58
kat <143475611+puzzlesremixed@users.noreply.github.com>
2026-02-07 23:27:46 +0700

    fix: pass edit message to dispatch req

--- src/services/api.ts
+++ src/services/api.ts
@@ -32,6 +32,7 @@ export const getPageCountFromPrinter = async (
   try {
     const formData = new FormData();
     formData.append("file", fileData, filename);
+    console.log(process.env.PAGE_CHECK_URL + "count-pages")
     const response = await fetch(process.env.PAGE_CHECK_URL + "count-pages", {
       method: "POST",
       body: formData,
@@ -62,7 +63,7 @@ export const detectColorCosts = async (
     const formData = new FormData();
     formData.append("files", fileData, filename);
 
-    const response = await fetch("http://localhost:5000/detect", {
+    const response = await fetch(process.env.COLOR_CHECK_URL + "detect", {
       method: "POST",
       body: formData,
     });
@@ -123,24 +124,24 @@ export const createPrintJob = async (chat: pkg.Chat, session: UserState) => {
   const contact = await client.getContactById(chat.id._serialized);
   formData.append("customer_name", session.customerName || "N/A");
   formData.append("customer_number", contact.number);
-
   session.files.forEach((file, index) => {
     formData.append(`items[${index}][file]`, file.data, file.filename);
     formData.append(`items[${index}][color]`, mapConfigToApiValue(file.config));
+    formData.append(`items[${index}][needs_edit]`, String(file.needsEdit));
+    formData.append(`items[${index}][pages]`, String(file.calculatedPages));
+
     if (file.paperSize)
       formData.append(`items[${index}][paper_size]`, file.paperSize);
     if (file.scale) formData.append(`items[${index}][scale]`, file.scale);
     if (file.side) formData.append(`items[${index}][side]`, file.side);
 
     if (file.copies)
-      formData.append(`items[${index}][copies-dummy]`, String(file.copies));
+      formData.append(`items[${index}][copies]`, String(file.copies));
     if (file.pagesToPrint)
-      formData.append(`items[${index}][pages-dummy]`, file.pagesToPrint);
-    if (file.needsEdit) {
-      formData.append(`items[${index}][needs_edit-dummy]`, "true");
-    }
+      formData.append(`items[${index}][pages_to_print]`, file.pagesToPrint);
+
     if (file.editNotes) {
-      formData.append(`items[${index}][edit_notes-dummy]`, file.editNotes);
+      formData.append(`items[${index}][edit_notes]`, file.editNotes);
     }
   });
 
